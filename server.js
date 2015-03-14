8b8e0fc7eb58b1093218d8e4f7cc362485a41680
Igor Stepanov <sigorilla@gmail.com>
2015-03-14 14:56:18 +0300

    Refactoring and writing doc

--- server.js
+++ server.js
@@ -10,42 +10,63 @@ var path, query, result;
 
 http.createServer( requestListener ).listen( port, host );
 
+/**
+ * Listener of server
+ * @param  {Object} req request object
+ * @param  {Object} res response object
+ */
 function requestListener ( req, res ) {
 	var parseURL = url.parse( req.url, true );
 	path = "db" + parseURL.pathname + ".json";
 	query, result;
-
-	fs.readFile( path, function ( err, data ) {
-		if ( err ) {
-			return showResult( res, 500, "Cannot read file '" + path + "'.", err, 500 );
-		}
-
-		var db = JSON.parse( data.toString() );
-		if ( db ) {
-			query = parseURL.query.query;
-			if ( query ) {
-				query = "$." + query;
-				result = {};
-				try {
-					result = jsonql( query, db );
-				} catch ( err ) {
-					return showResult( res, 500, "Something wrong with 'jsonql'.", err );
-				}
-			} else {
-				return showResult( res, 404, "No attribute 'query' in your query." );
-			}
-		} else {
-			return showResult( res, 404, "DB is empty." );
-		}
-
-		if ( typeof result !== "undefined" ) {
-			return showResult( res );
-		} else {
-			return showResult( res, 404, "Nothing is found for query: '" + query + "'." );
-		}
-	} );
+	fs.readFile( path, readCallback );
 }
 
+/**
+ * Callback function for reading file
+ * @param  {Object} err  object with information about error
+ * @param  {String} data buffer string from file
+ * @return {Object}      Signal of end for response.end()
+ */
+function readCallback ( err, data ) {
+	if ( err ) {
+		return showResult( res, 500, "Cannot read file '" + path + "'.", err, 500 );
+	}
+
+	var db = JSON.parse( data.toString() );
+	if ( db ) {
+		query = parseURL.query.query;
+		if ( query ) {
+			query = "$." + query;
+			result = {};
+			try {
+				result = jsonql( query, db );
+			} catch ( err ) {
+				return showResult( res, 500, "Something wrong with 'jsonql'.", err );
+			}
+		} else {
+			return showResult( res, 404, "No attribute 'query' in your query." );
+		}
+	} else {
+		return showResult( res, 404, "DB is empty." );
+	}
+
+	if ( typeof result !== "undefined" ) {
+		return showResult( res );
+	} else {
+		return showResult( res, 404, "Nothing is found for query: '" + query + "'." );
+	}
+}
+
+/**
+ * Show result on page
+ * @param  {Object}  res    object from requestListner
+ * @param  {Int}     code   code to header
+ * @param  {String}  data   message about error or result of jsonql
+ * @param  {Object}  err    object with information about error
+ * @return {Object}         Signal to the server that all of the response headers and body 
+ *                          have been sent
+ */
 function showResult ( res, code, data, err ) {
 	code = code || 200;
 	data = data || result;
